5caa33b328a9988414e1b92a4495b7d3bade3558
Aaron King <kingaaron2003@gmail.com>
2025-10-06 12:03:24 -0500

    Added Corpus Generator

--- CleanArticle.py
+++ CleanArticle.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from pathlib import Path
 
+import CorpusGenerator as cg
 
-base = Path(r"C:/Users/Justin/OneDrive/Documents/ArticleAI_Data")
-csv_path = base / "data.csv"
+base = Path.cwd()
+csv_path = base / "Articles.csv"
 
 # --- 1) Load CSV and normalize ---
 df = (pd.read_csv(csv_path)[["Content", "Summary"]].rename(columns={"Content": "article", "Summary": "summary"})
@@ -22,6 +23,13 @@ df = df.drop_duplicates(subset=["article", "summary"], keep="first").reset_index
 
 print(df.info())
 print(df.head(10))
+
+all_text = " ".join(df["article"].astype(str).tolist())
+
+corpus_dir = base / "corpus"
+corpus_dir.mkdir(exist_ok=True)
+
+tokens, token2idx, idx2token = cg.process_corpus(corpus_dir=str(corpus_dir), text=all_text)
 
 # save
 #out_csv = base / "final_news_summary.csv"
